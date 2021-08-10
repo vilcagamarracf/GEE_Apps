c4646948b9ad41d5e7afec02913fac806daf8c53
Cesar Vilca <vilcagamarracf@gmail.com>
2021-08-09 23:08:07 -0500

    Update 🚀

--- 01 Landsat8 Filter/01_Filtrado.js
+++ 01 Landsat8 Filter/01_Filtrado.js
@@ -1,8 +1,10 @@
 // MEJORANDO CODIGO: Landsat8: Filter 
 
 // Google Earth Engine Tutorial: Widgets and Apps     https://www.youtube.com/watch?v=ctLsxPXr76A
+// Ejemplos de apps:
 // https://github.com/jdbcode/Snazzy-EE-TS-GIF/blob/master/script/ee-ts-gif.js
 // https://emaprlab.users.earthengine.app/view/lt-gee-time-series-animator
+// https://github.com/Andi1974/Forest-degradation-monitoring/blob/2.4.1/Delta-rNBR.txt
 
 ui.root.clear();
 
@@ -57,99 +59,119 @@ var filterPanel = ui.Panel({
 });
 
 // Panel derecho: Funciones 1, 2 y 3 -------------------------------------------
-var fecha_ini = ui.Textbox({placeholder: 'YYYY-MM-DD', value: '2020-01-01', style: BUTTON_STYLE});
-var fecha_fin = ui.Textbox({placeholder: 'YYYY-MM-DD', value: '2020-12-31', style: BUTTON_STYLE});
-var nubesText = ui.Textbox({placeholder: 'NN', value: '20', style: BUTTON_STYLE});
+var fecha_ini = ui.Textbox({placeholder: 'YYYY-MM-DD', value: '2020-01-01', style: BUTTON_STYLE, 
+  onChange: function(text){
+    var fecha_ini_chng = text
+  }});
+var fecha_fin = ui.Textbox({placeholder: 'YYYY-MM-DD', value: '2020-12-31', style: BUTTON_STYLE,
+  onChange: function(text){
+    var fecha_fin_chng = text
+  }});
+
+var nubesText = ui.Slider({
+  min: 0,
+  max: 100, 
+  value: 20, 
+  step: 5,
+  onChange: function(value) {
+    var nubesText_chng = value
+  },
+  style: {width: '215px'}
+});
+
+// Panel derecho: Funcion 4 ------------------------------------------------------
+
+var mapPanel = ui.Map();
+
+var DEPARTAMENTOS = ee.FeatureCollection('users/CesarVilca/Departamentos_Peru');
+var VIZ_PARAMS = {bands: ['B4', 'B3', 'B2'], min: 0, max: 0.4, gamma: 1.5};
+
+var selectDepartamentos = ui.Select({
+  items: [
+    {label:'AMAZONAS', value:'AMAZONAS'},
+    {label:'ANCASH', value:'ANCASH'},
+    {label:'APURIMAC', value:'APURIMAC'},
+    {label:'AREQUIPA', value:'AREQUIPA'},
+    {label:'AYACUCHO', value:'AYACUCHO'},
+    {label:'CAJAMARCA', value:'CAJAMARCA'},
+    {label:'CUSCO', value:'CUSCO'},
+    {label:'HUANCAVELICA', value:'HUANCAVELICA'},
+    {label:'HUANUCO', value:'HUANUCO'},
+    {label:'ICA', value:'ICA'},
+    {label:'JUNIN', value:'JUNIN'},
+    {label:'LA LIBERTAD', value:'LA LIBERTAD'},
+    {label:'LAMBAYEQUE', value:'LAMBAYEQUE'},
+    {label:'LIMA', value:'LIMA'},
+    {label:'LORETO', value:'LORETO'},
+    {label:'MADRE DE DIOS', value:'MADRE DE DIOS'},
+    {label:'MOQUEGUA', value:'MOQUEGUA'},
+    {label:'PASCO', value:'PASCO'},
+    {label:'PIURA', value:'PIURA'},
+    {label:'PUNO', value:'PUNO'},
+    {label:'SAN MARTIN', value:'SAN MARTIN'},
+    {label:'TACNA', value:'TACNA'},
+    {label:'TUMBES', value:'TUMBES'},
+    {label:'UCAYALI', value:'UCAYALI'}
+    ], 
+  placeholder: 'Departamentos',
+  onChange: function(value) {
+    var departamentos_chng = value
+  },
+  style: BUTTON_STYLE
+})
 
 filterPanel.add(fecha_ini);
 filterPanel.add(fecha_fin);
 filterPanel.add(nubesText);
-
-// Filtrado (REVISAR)
-var COLLECTION_ID = 'LANDSAT/LC08/C01/T1_SR';
-var START_DATE = '2020-01-01';
-var END_DATE = '2021-01-01';
-var NUBOSIDAD = 20;
-
-var BASE_COLLECTION = ee.ImageCollection(COLLECTION_ID)
-                        .filter(ee.Filter.lte('CLOUD_COVER', NUBOSIDAD))
-                        .filterDate(START_DATE, END_DATE);
-
-
-// Panel derecho: Funcion 4 ------------------------------------------------------
-
-var DEPARTAMENTOS = ee.FeatureCollection('users/CesarVilca/Departamentos_Peru');
-
-var departamentos = {
-  'AMAZONAS':['AMAZONAS'],
-  'ANCASH':['ANCASH'],
-  'APURIMAC':['APURIMAC'],
-  'AREQUIPA':['AREQUIPA'],
-  'AYACUCHO':['AYACUCHO'],
-  'CAJAMARCA':['CAJAMARCA'],
-  // 'CALLAO':['CALLAO'],
-  'CUSCO':['CUSCO'],
-  'HUANCAVELICA':['HUANCAVELICA'],
-  'HUANUCO':['HUANUCO'],
-  'ICA':['ICA'],
-  'JUNIN':['JUNIN'],
-  'LA LIBERTAD':['LA LIBERTAD'],
-  'LAMBAYEQUE':['LAMBAYEQUE'],
-  'LIMA':['LIMA'],
-  'LORETO':['LORETO'],
-  'MADRE DE DIOS':['MADRE DE DIOS'],
-  'MOQUEGUA':['MOQUEGUA'],
-  'PASCO':['PASCO'],
-  'PIURA':['PIURA'],
-  'PUNO':['PUNO'],
-  'SAN MARTIN':['SAN MARTIN'],
-  'TACNA':['TACNA'],
-  'TUMBES':['TUMBES'],
-  'UCAYALI':['UCAYALI']
-}
-
-var VIZ_PARAMS = {bands: ['B4', 'B3', 'B2'], min: 0, max: 0.4, gamma: 1.5};
-
-
-var mapPanel = ui.Map();
-
-var selectDepartamentos = ui.Select({
-  items: Object.keys(departamentos), 
-  placeholder: 'Departamentos',
-  onChange: function(key, icol){
-    
-    mapPanel.clear() // Reinicia la visualización
-    
-    // Filtrado por departamento
-    var filtrado = DEPARTAMENTOS.filter(ee.Filter.eq('NOMBDEP', departamentos[key][0])).geometry()
-    var viz = BASE_COLLECTION.filterBounds(filtrado).median().multiply(0.0001)
-    
-    mapPanel.addLayer(viz, VIZ_PARAMS, 'Imagenes Landsat8'); // Agregar imágenes Landsat8
-
-    // Departamento seleccionado: Asignar un color
-    var bordeDpto = ee.Image().byte().paint({
-      featureCollection: filtrado,
-      color: 1,
-      width: 2
-    });
-    
-    mapPanel.addLayer(bordeDpto, {palette: '#24C1E0'}, departamentos[key][0]); // Agregar layer del departamento
-    
-    mapPanel.centerObject(filtrado, 7)
-    },
-  style: BUTTON_STYLE
-})
-
 filterPanel.add(selectDepartamentos);
 
 // Botón de RUN
 var button = ui.Button({
   label: 'Run', 
+  onClick: function(){return runFilter()},
   style: BUTTON_STYLE
-  // onClick, disabled, style})
 })
 
 filterPanel.add(button);
+
+var runFilter = function(key, icol){
+  mapPanel.clear() // Reinicia la visualización  var fecha_ini_chng = fecha_ini.getValue()
+  
+  var fecha_ini_chng = fecha_ini.getValue()
+  var fecha_fin_chng = fecha_fin.getValue()
+  var nubesText_chng = nubesText.getValue()
+  var departamentos_chng = selectDepartamentos.getValue()
+  
+  if  (departamentos_chng === null){
+    // var boton = ui.Button('Escoge un departamento');
+    //   boton.style().set({
+    //     position: 'top-center',
+    //     border : '1px solid #000000',
+    //   });
+    //   Map.add(boton);
+    mapPanel.addLayer(DEPARTAMENTOS.geometry(),{},'Departamentos del Perú')
+  } else {
+    // Filtrado por departamento
+  var filtrado = DEPARTAMENTOS.filterMetadata('NOMBDEP','equals', departamentos_chng).geometry()
+  var COLLECTION_ID = 'LANDSAT/LC08/C01/T1_SR';
+  var viz = ee.ImageCollection(COLLECTION_ID)
+                          .filter(ee.Filter.lte('CLOUD_COVER', nubesText_chng))
+                          .filterDate(fecha_ini_chng, fecha_fin_chng)
+                          .filterBounds(filtrado).median().multiply(0.0001)
+
+  mapPanel.addLayer(viz, VIZ_PARAMS, 'Imagenes Landsat8'); // Agregar imágenes Landsat8
+
+  // Departamento seleccionado: Asignar un color
+  var bordeDpto = ee.Image().byte().paint({
+    featureCollection: filtrado,
+    color: 1,
+    width: 2
+  });
+  
+  mapPanel.addLayer(bordeDpto, {palette: '#24C1E0'}, departamentos_chng); // Agregar layer del departamento
+  mapPanel.centerObject(filtrado, 7)
+  }
+}
 
 // Estableciendo la estructura final
 
